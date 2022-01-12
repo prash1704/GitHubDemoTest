3bda0853a3ff489824507d2c31dd3d373182b9de
prash1704 <prashant.1704@gmail.com>
2022-01-12 18:51:34 +0530

    'firstdraft'

---
+++ GITHUBDemoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GITHUBDemoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
